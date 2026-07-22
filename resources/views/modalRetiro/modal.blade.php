9efaaa99952b6b2dd36869d3aa0c0e8998b572ab
Lionel Carreras <lionel.carreras.nob@gmail.com>
2026-07-22 16:23:40 -0300

    Close retiro modal before page footer

--- resources/views/modalRetiro/modal.blade.php
+++ resources/views/modalRetiro/modal.blade.php
@@ -247,7 +247,7 @@
             </div>
             </div>
 
-
+        </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
           <button type="button" id="guardarModal" class="btn btn-primary">Guardar</button>
